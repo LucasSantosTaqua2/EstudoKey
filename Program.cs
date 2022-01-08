d7b2188c02997b728e1bcf39851f96eaff380a10
Lucas Santos <lucassantosdev@hotmail.com>
2022-01-08 17:05:50 -0300

    Final Commit

--- Program.cs
+++ Program.cs
@@ -8,67 +8,74 @@ namespace Vs
     {
         public static void Main(string[] args)
         {
-
+            
            
 
-            Console.WriteLine("Escolha uma opção: 1- Cidades, 2- Paises, 3- Continentes, 0- fechar");
+           /* Console.WriteLine("Escolha uma opção: 1- Cidades, 2- Paises, 3- Continentes, 0- fechar");
             Console.WriteLine("Digite a opção: ");
             ConsoleKeyInfo input = Console.ReadKey();
-            int result = int.Parse(input.KeyChar.ToString());
-            Console.WriteLine(input.KeyChar);
+            int result = int.Parse(input.KeyChar.ToString());*/
+
             
+                int result = 0;
             
-            do{ if (result == 1){
-                Console.WriteLine();
-                Console.WriteLine("ESCOLHEU OPÇÃO UM - CIDADES!");
-                Console.WriteLine();
-                Console.WriteLine("Aqui estão algumas cidades do mundo.");
-                Console.WriteLine("____________________");
-                Console.WriteLine();
-                Console.WriteLine("Londres");
-                Console.WriteLine("Nova York");
-                Console.WriteLine("São Paulo");
-                Console.WriteLine("São Petersburgo");
-                Console.WriteLine("Tokyo");
-                Console.WriteLine("____________________");
-            }
+            do
+            {
+                Console.WriteLine("Escolha uma opção: 1- Cidades, 2- Paises, 3- Continentes, 0- fechar");
+                Console.WriteLine("Digite a opção: ");
+                ConsoleKeyInfo input = Console.ReadKey();
+                result = int.Parse(input.KeyChar.ToString());
+            
 
-            else if (result == 2){
-                Console.WriteLine();
-                Console.WriteLine("ESCOLHEU OPÇÃO DOIS - PAISES!");
-                Console.WriteLine();
-                Console.WriteLine("Aqui estão alguns paises do mundo.");
-                Console.WriteLine("____________________");
-                Console.WriteLine();
-                Console.WriteLine("Estados Unidos");
-                Console.WriteLine("Rússia");
-                Console.WriteLine("Peru");
-                Console.WriteLine("Holanda");
-                Console.WriteLine("Brasil");
-                Console.WriteLine("____________________"); 
-            }
 
-            else if (result == 3){
-                Console.WriteLine();
-                Console.WriteLine("ESCOLHEU OPÇÃO TRÊS - CONTINENTES!");
-                Console.WriteLine();
-                Console.WriteLine("Aqui estão alguns continentes do mundo.");
-                Console.WriteLine("____________________");
-                Console.WriteLine();
-                Console.WriteLine("Ásia");
-                Console.WriteLine("África");
-                Console.WriteLine("Oceania");
-                Console.WriteLine("America");
-                Console.WriteLine("Europa");
-                Console.WriteLine("____________________"); 
-            } 
+            
+                if (result == 1)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("ESCOLHEU OPÇÃO UM - CIDADES!");
+                    Console.WriteLine();
+                    Console.WriteLine("Aqui estão algumas cidades do mundo.");
+                    Console.WriteLine("________");
+                    Console.WriteLine();
+                    Console.WriteLine("Londres");
+                    Console.WriteLine("Nova York");
+                    Console.WriteLine("São Paulo");
+                    Console.WriteLine("São Petersburgo");
+                    Console.WriteLine("Tokyo");
+                    Console.WriteLine("________");
+                    
+                    
+                }else if (result == 2)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("ESCOLHEU OPÇÃO DOIS - PAISES!");
+                    Console.WriteLine();
+                    Console.WriteLine("Aqui estão alguns paises do mundo.");
+                    Console.WriteLine("________");
+                    Console.WriteLine();
+                    Console.WriteLine("Estados Unidos");
+                    Console.WriteLine("Rússia");
+                    Console.WriteLine("Peru");
+                    Console.WriteLine("Holanda");
+                    Console.WriteLine("Brasil");
+                    Console.WriteLine("________");
+                }else if (result == 3)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("ESCOLHEU OPÇÃO TRÊS - CONTINENTES!");
+                    Console.WriteLine();
+                    Console.WriteLine("Aqui estão alguns continentes do mundo.");
+                    Console.WriteLine("________");
+                    Console.WriteLine();
+                    Console.WriteLine("Ásia");
+                    Console.WriteLine("África");
+                    Console.WriteLine("Oceania");
+                    Console.WriteLine("America");
+                    Console.WriteLine("Europa");
+                    Console.WriteLine("________");
+                }
 
-            else if (result == 0){
-                    Environment.Exit(0);
-            }
-
-           }while (result == 0);       
-
+            } while (result != 0);
 
 
 
