c6f6459ca257637c9e8eef01fc9cf39a5e0631a8
Lucas Santos <lucassantosdev@hotmail.com>
2022-01-08 12:36:42 -0300

    master

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Vs 
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Escolha uma opção: 1- Cidades, 2- Paises, 3- Estados");
+            Console.WriteLine("Digite a opção: ");
+            ConsoleKeyInfo input = Console.ReadKey();
+            int result = int.Parse(input.KeyChar.ToString());
+            Console.WriteLine();
+            Console.WriteLine("Você escolheu: ", result);
+            
+            if (result == 1){
+                Console.WriteLine("ESCOLHEU OPÇÃO UM!");
+            }
+        }
+    }
+}
